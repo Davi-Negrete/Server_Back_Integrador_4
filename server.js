c267feb318e5f188a35e7f5e66e4475f948eb5bc
Davi-Negrete <luisdavidnegrete12@gmail.com>
2024-10-04 21:02:10 -0500

    Creo el archivo productos.router dentro de la carpeta routers y gestiono todo el ruteo de los productos

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import 'dotenv/config'
+import routerProductos from './routers/productos.router.js'
+
+// ! Variables/Constantes
+
+const app = express()
+const PORT = process.env.PORT || 2222
+
+// ! MidleWares
+
+app.use(express.json()) //Interpreta el body que le pasamos y lo entiende
+
+// ! Rutas
+
+app.use('/api/v1/productos', routerProductos)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.all('*', (req, res) => {
+    res
+        .status(404)
+        .json({
+            ruta: `${req.url}`,
+            metodo: `${req.method}`,
+            mensaje: 'No se pudo acceder al recurso solicitado'
+        })
+})
+
+app.listen(PORT, (error) => {
+    if (error) throw new Error('No se pudo levantar el servidor', Error)
+  console.log(`Servidor corriendo en: http://localhost:${PORT}`)
+})
+
